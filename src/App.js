bb6e7f5ac7f6f43773f7a17f5b975272bd9f025a
Nishika Agarwal <nishika991@gmail.com>
2022-05-30 19:11:50 +0530

    Initial components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './Components/Layout/Header'
+import { Fragment } from 'react';
+import Meals from './Components/Meals/Meals';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>  
+
+  );
+}
+
+export default App;
