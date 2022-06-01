4ca36046a925c99e65f667efe9382d5d12c28d4f
Nishika Agarwal <nishika991@gmail.com>
2022-06-01 22:33:55 +0530

    update

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>ReactMeals</h1>
+                <h1>ReactMeals Application</h1>
                 <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
